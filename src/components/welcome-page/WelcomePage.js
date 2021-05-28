89b17021d4b7f01052b923bf0d80244c70d31de5
Cristian Nicolae <nicolae.cristianmihai@gmail.com>
2021-05-28 18:21:49 +0300

    Almost final layout

--- src/components/welcome-page/WelcomePage.js
+++ src/components/welcome-page/WelcomePage.js
@@ -1,11 +1,23 @@
-import './WelcomePage.scss';
-
+import "./WelcomePage.scss";
+import TextField from "@material-ui/core/TextField";
 function WelcomePage() {
-    return(
-        <div className="wp-container">
-            <div className="wp-title">Welcome to the chat</div>
+  return (
+    <div className="wp-container">
+      <div className="wp-title">Welcome to the chat</div>
+      <div className="wp-form-cointainer">
+        <div className="wp-form-label">Enter the secret key:</div>
+        <div>
+          <TextField
+            required
+            variant="outlined"
+            id="wp-key-form"
+            placeholder="SAFEPASSWORD..."
+          />
         </div>
-    );
+      </div>
+      <button>Join now!</button>
+    </div>
+  );
 }
 
 export default WelcomePage;
