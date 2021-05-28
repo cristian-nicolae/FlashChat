f7c578bef6d8c0058b8a9fde1ae1c812e83d1dc5
Cristian Nicolae <nicolae.cristianmihai@gmail.com>
2021-05-28 17:08:46 +0300

    Testing modules

---
+++ src/components/welcome-page/WelcomePage.js
@@ -0,0 +1,11 @@
+import './WelcomePage.scss';
+
+function WelcomePage() {
+    return(
+        <div className="wp-container">
+            <div className="wp-title">Welcome to the chat</div>
+        </div>
+    );
+}
+
+export default WelcomePage;
