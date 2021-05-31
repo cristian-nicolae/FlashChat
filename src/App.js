4a0687be40fb6754cec533e622b4a8d68ecc1678
Cristian Nicolae <nicolae.cristianmihai@gmail.com>
2021-06-01 00:31:06 +0300

    Added firebase app config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import './App.scss';
-import MainView from './components/main-view/MainView'
+import MainView from './components/main-view/MainView';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAGDSdzZF4xDDtHulNeLJPeZcJth-hM5fE",
+  authDomain: "flashchat-dt.firebaseapp.com",
+  projectId: "flashchat-dt",
+  storageBucket: "flashchat-dt.appspot.com",
+  messagingSenderId: "509568857421",
+  appId: "1:509568857421:web:aeb6dad4e98a91a930b304",
+  measurementId: "G-0FWMQZTBBW"
+})
+
+const firestore = firebase.firestore();
 
 function App() {
   return (
