5c428236bc053c9734e327084fb986b23bdfba3e
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-05-31 19:50:41 +0300

    Update Chat.js

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, {useEffect} from "react";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-function Chat(){
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+function Chat() {
+
+    useEffect( ()=>{
+        // firebase.initializeApp({
+
+        // });
+        
+        // const firestore = firebase.firestore();
+    })
     return (
         <div>Chat</div>
     )
